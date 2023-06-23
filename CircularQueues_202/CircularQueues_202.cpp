e544a21448c09091eca271e9807faa323cd07531
wawanpi <muhaguskurniawan@gmail.com>
2023-06-23 11:28:43 +0700

    done

--- CircularQueues_202/CircularQueues_202.cpp
+++ CircularQueues_202/CircularQueues_202.cpp
@@ -125,4 +125,15 @@ int main() {
 			case '4': {
 				return 0;
 			}
-			
+			default: {
+				cout << "Invalid option !!" << endl;
+				break;
+			}
+			}
+		}
+		catch (exception& e) {
+			cout << "Check for the values entered." << endl;
+		}
+	}
+	return 0;
+}
