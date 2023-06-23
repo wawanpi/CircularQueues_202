ff6ba4db2fff8435fd7256105c001348b7e41d3a
wawanpi <muhaguskurniawan@gmail.com>
2023-06-23 11:26:57 +0700

    iterasi dari awal array hingga REAR

--- CircularQueues_202/CircularQueues_202.cpp
+++ CircularQueues_202/CircularQueues_202.cpp
@@ -76,4 +76,19 @@ public:
 			}
 			cout << endl;
 		}
-		
+		else
+			//jika FRONT_position > REAR_position, iterasi dari FRONT hingg akhir array
+			while (FRONT_position <= n - 1) {
+				cout << queue_array[FRONT_position] << "    ";
+				FRONT_position++;
+			}
+		FRONT_position = 0;
+
+		//iterasi dari awal array hingga REAR
+		while (FRONT_position <= REAR_position) {
+			cout << queue_array[FRONT_position] << "   ";
+			FRONT_position++;
+		}
+		cout << endl;
+	}
+};
