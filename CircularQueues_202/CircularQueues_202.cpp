72c36197dcdc2883b3324173efef8518b7328295
wawanpi <muhaguskurniawan@gmail.com>
2023-06-23 11:23:40 +0700

    membuat method display

--- CircularQueues_202/CircularQueues_202.cpp
+++ CircularQueues_202/CircularQueues_202.cpp
@@ -1,34 +1,33 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Queuse {
+class Queues {
 	int FRONT, REAR, n = 5;
 	int queue_array[5];
 
 public:
-	Queuse() {
+	Queues() {
 		FRONT = -1;
 		REAR = -1;
 	}
-
 	void insert() {
 		int num;
 		cout << "Enter a number: ";
 		cin >> num;
 		cout << endl;
 
-		// cek apakah antrian penuh
-		if ((FRONT == 0 && REAR == n - 1)) || (FRONT == REAR + 1)) {
+		//cek apakah antrian penuh
+		if ((FRONT == 0 && REAR == n - 1) || (FRONT == REAR + 1)) {
 			cout << "\nQueue overflow\n";
 			return;
 		}
-		// cek apakah antrian kosong
+		//cek apakah antrian kosong
 		if (FRONT == -1) {
 			FRONT = 0;
 			REAR = 0;
 		}
 		else {
-			// jika REAR berada di posisi terakhir array, kembali ke awal array
+			//jika rear berada do posisi terakhir arrray, kembali ke awal array
 			if (REAR == n - 1)
 				REAR = 0;
 			else
@@ -36,13 +35,27 @@ public:
 		}
 		queue_array[REAR] = num;
 	}
+
 	void remove() {
 		//cek apakah antrian kosong
 		if (FRONT == -1) {
-			cout << "Queue underflow/n";
+			cout << "Queue underflow\n";
 			return;
 		}
-		cout << "\nThe element deleted from the queuw isi :" << queue_array[FRONT] << "\n";
+		cout << "\nThe element deleted from the queue is : " << queue_array[FRONT] << "\n";
 
+		//cek jika antrian hanya memiliki satu elemen
+		if (FRONT == REAR) {
+			FRONT = -1;
+			REAR = -1;
+		}
+		else {
+			//jika elemen yang dihapus berda di posisi terakhir array. kembali ke awal array
+			if (FRONT == n - 1)
+				FRONT = 0;
+			else
+				FRONT = FRONT + 1;
+		}
 	}
-};
+	void display() {
+		
